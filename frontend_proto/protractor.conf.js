385eda73ea3d37a83c5e352e91c269ee20adaf83
Alex Beugnet <alex.beugnet@akka.eu>
2017-10-27 19:31:59 +0200

    update protractor conf

--- frontend_proto/protractor.conf.js
+++ frontend_proto/protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     'browserName': 'chrome',
     'chromeOptions': {
       // 'args': ['show-fps-counter=true', '--no-sandbox']
-      'args': ['--headless', 'no-sandbox', '--disable-gpu', '--window-size=800x600']
+      'args': ['--headless', '--disable-gpu', '--window-size=800x600']
     },
   },
   directConnect: true,
