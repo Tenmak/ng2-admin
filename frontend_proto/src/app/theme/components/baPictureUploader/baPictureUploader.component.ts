eac5af073a4fcc2060b8ffec99a65780b50c94ca
Alex Beugnet <alex.beugnet@akka.eu>
2017-11-07 18:05:24 +0100

    NG-CLI-V1.5; NGV5.0.0; Upgrade global packages to lastest stable version;

--- frontend_proto/src/app/theme/components/baPictureUploader/baPictureUploader.component.ts
+++ frontend_proto/src/app/theme/components/baPictureUploader/baPictureUploader.component.ts
@@ -1,76 +1,76 @@
-import {Component, ViewChild, Input, Output, EventEmitter, ElementRef, Renderer} from '@angular/core';
-import { NgUploaderOptions } from 'ngx-uploader';
+// import {Component, ViewChild, Input, Output, EventEmitter, ElementRef, Renderer} from '@angular/core';
+// import { NgUploaderOptions } from 'ngx-uploader';
 
-@Component({
-  selector: 'ba-picture-uploader',
-  styleUrls: ['./baPictureUploader.scss'],
-  templateUrl: './baPictureUploader.html',
-})
-export class BaPictureUploader {
+// @Component({
+//   selector: 'ba-picture-uploader',
+//   styleUrls: ['./baPictureUploader.scss'],
+//   templateUrl: './baPictureUploader.html',
+// })
+// export class BaPictureUploader {
 
-  @Input() defaultPicture:string = '';
-  @Input() picture:string = '';
+//   @Input() defaultPicture:string = '';
+//   @Input() picture:string = '';
 
-  @Input() uploaderOptions:NgUploaderOptions = { url: '' };
-  @Input() canDelete:boolean = true;
+//   @Input() uploaderOptions:NgUploaderOptions = { url: '' };
+//   @Input() canDelete:boolean = true;
 
-  @Output() onUpload = new EventEmitter<any>();
-  @Output() onUploadCompleted = new EventEmitter<any>();
+//   @Output() onUpload = new EventEmitter<any>();
+//   @Output() onUploadCompleted = new EventEmitter<any>();
 
-  @ViewChild('fileUpload') public _fileUpload:ElementRef;
+//   @ViewChild('fileUpload') public _fileUpload:ElementRef;
 
-  public uploadInProgress:boolean;
+//   public uploadInProgress:boolean;
 
-  constructor(private renderer: Renderer) {
-  }
+//   constructor(private renderer: Renderer) {
+//   }
 
-  beforeUpload(uploadingFile): void {
-    let files = this._fileUpload.nativeElement.files;
+//   beforeUpload(uploadingFile): void {
+//     let files = this._fileUpload.nativeElement.files;
 
-    if (files.length) {
-      const file = files[0];
-      this._changePicture(file);
+//     if (files.length) {
+//       const file = files[0];
+//       this._changePicture(file);
 
-      if (!this._canUploadOnServer()) {
-        uploadingFile.setAbort();
-      } else {
-        this.uploadInProgress = true;
-      }
-    }
-  }
+//       if (!this._canUploadOnServer()) {
+//         uploadingFile.setAbort();
+//       } else {
+//         this.uploadInProgress = true;
+//       }
+//     }
+//   }
 
-  bringFileSelector():boolean {
-    this.renderer.invokeElementMethod(this._fileUpload.nativeElement, 'click');
-    return false;
-  }
+//   bringFileSelector():boolean {
+//     this.renderer.invokeElementMethod(this._fileUpload.nativeElement, 'click');
+//     return false;
+//   }
 
-  removePicture():boolean {
-    this.picture = '';
-    return false;
-  }
+//   removePicture():boolean {
+//     this.picture = '';
+//     return false;
+//   }
 
-  _changePicture(file:File):void {
-    const reader = new FileReader();
-    reader.addEventListener('load', (event:Event) => {
-      this.picture = (<any> event.target).result;
-    }, false);
-    reader.readAsDataURL(file);
-  }
+//   _changePicture(file:File):void {
+//     const reader = new FileReader();
+//     reader.addEventListener('load', (event:Event) => {
+//       this.picture = (<any> event.target).result;
+//     }, false);
+//     reader.readAsDataURL(file);
+//   }
 
-  _onUpload(data):void {
-    if (data['done'] || data['abort'] || data['error']) {
-      this._onUploadCompleted(data);
-    } else {
-      this.onUpload.emit(data);
-    }
-  }
+//   _onUpload(data):void {
+//     if (data['done'] || data['abort'] || data['error']) {
+//       this._onUploadCompleted(data);
+//     } else {
+//       this.onUpload.emit(data);
+//     }
+//   }
 
-  _onUploadCompleted(data):void {
-    this.uploadInProgress = false;
-    this.onUploadCompleted.emit(data);
-  }
+//   _onUploadCompleted(data):void {
+//     this.uploadInProgress = false;
+//     this.onUploadCompleted.emit(data);
+//   }
 
-  _canUploadOnServer():boolean {
-    return !!this.uploaderOptions['url'];
-  }
-}
+//   _canUploadOnServer():boolean {
+//     return !!this.uploaderOptions['url'];
+//   }
+// }
